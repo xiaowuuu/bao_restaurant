e5544126b3d4a5e6a7bd6cdeb1bfc0015aaf4e98
ying <huaiy1105@gmail.com>
2023-07-28 13:32:14 +0100

    update app.py. add flaskenv file

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user@localhost:5432/bao_database"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route("/")
+def home():
+    return "welcome to our Bao house."
