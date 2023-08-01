ad7a6ba55f2716e8483c3eede0041c009f9bb29c
ying <huaiy1105@gmail.com>
2023-08-01 21:43:06 +0100

    add create new user feature

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
-from models import User, Item, Order, OrderItem
+from models import User
 from app import db
 
 users_blueprint = Blueprint("users", __name__)
 
-@users_blueprint.route("/users/new", methods=["POST"])
+@users_blueprint.route("/users/add")
+def add_user():
+    return render_template("users/new_user.jinja")
+
+@users_blueprint.route("/users/add", methods=["GET","POST"])
 def new_user():
     name = request.form['name']
     phone = request.form['phone']
     address = request.form['address']
-    db.session
+    user = User(name=name, phone=phone, address=address)
+    db.session.add(user)
+    db.session.commit()
+    return redirect("/users")
